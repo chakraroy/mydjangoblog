96eb844407c146229ee67a0c4a8c704b28b6b26b
Chakra Roy <chakraroy@gmail.com>
2019-01-20 00:48:54 +0800

    section3 templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Charox',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 19, 2019',
+    },
+    {
+        'author': 'Ali Roy',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 20, 2019',
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
