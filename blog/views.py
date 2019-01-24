a4a7d7dce18b3e84ef982b7cb71416d3a1b5b46e
Chakra Roy <chakraroy@gmail.com>
2019-01-24 12:45:10 +0800

    Wow its section5

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Charox',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 19, 2019',
-    },
-    {
-        'author': 'Ali Roy',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 20, 2019',
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
